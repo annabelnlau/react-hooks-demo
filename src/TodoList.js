24babbecf8afc0d14c459695fa0d327ae97cb5dd
Annabel Lau <alau@fuzzproductions.com>
2019-09-28 16:10:30 -0400

    Build simple Todo List app

---
+++ src/TodoList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyledContainer, StyledTodoList, StyledInfo, StyledForm } from './styles';
+import { Button, Icon, Input, List } from 'antd';
+
+class TodoList extends React.Component {
+  state = {
+    todoItems: [],
+    inputValue: '',
+  }
+
+  handleChange = e => {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  addToDoItem = e => {
+    e.preventDefault();
+    const { todoItems, inputValue } = this.state;
+
+    const newTodoItems = todoItems.concat(inputValue);
+
+    this.setState({
+      todoItems: newTodoItems,
+      inputValue: ''
+    });
+  }
+
+  render() {
+    const { todoItems, inputValue } = this.state;
+    return (
+      <StyledContainer>
+        <StyledInfo>
+          <StyledTodoList>
+            <List
+              size="large"
+              header={<h1 style={{ margin: 0 }}><Icon type="pushpin" /> To-Do List</h1>}
+              bordered
+              dataSource={todoItems}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+          </StyledTodoList>
+          <StyledForm onSubmit={e => this.addToDoItem(e)}>
+            <Input
+              value={inputValue}
+              onChange={e => this.handleChange(e)}
+              style={{ marginRight: 10 }}
+              placeholder="What do you need to do?"
+            />
+            <Button type="submit">
+              <Icon type="plus" />
+            </Button>
+          </StyledForm>
+        </StyledInfo>
+      </StyledContainer>
+    );
+  }
+}
+
+export default TodoList;
---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  justify-content: center;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+export const StyledInfo = styled.div`
+  display: block;
+  width: 700px;
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  padding-top: 20px;
+`;
+
+export const StyledTodoList = styled.div`
+  text-align: center;
+`;
