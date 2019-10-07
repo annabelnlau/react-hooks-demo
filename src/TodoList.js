a522c67e15de912c63423be3eb7f83b576b2548a
Annabel Lau <alau@fuzzproductions.com>
2019-10-07 12:31:07 -0400

    Refactor

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,62 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledContainer, StyledTodoList, StyledInfo, StyledForm } from './styles';
 import { Button, Icon, Input, List } from 'antd';
 
-class TodoList extends React.Component {
-  state = {
-    todoItems: [],
-    inputValue: '',
-  }
+const TodoList = () => {
+  const [todoItems, addToDoItem] = useState([]);
+  const [inputValue, handleChange] = useState('');
 
-  componentDidMount(){
+  useEffect(() => {
     alert('Welcome!')
-  }
+  }, []);
 
-  handleChange = e => {
-    this.setState({ inputValue: e.target.value });
-  }
-
-  addToDoItem = e => {
+  const addItem = e => {
     e.preventDefault();
-    const { todoItems, inputValue } = this.state;
-
-    const newTodoItems = todoItems.concat(inputValue);
-
-    this.setState({
-      todoItems: newTodoItems,
-      inputValue: ''
-    });
+    addToDoItem([...todoItems, inputValue]);
+    handleChange('');
   }
 
-  render() {
-    const { todoItems, inputValue } = this.state;
-    return (
-      <StyledContainer>
-        <StyledInfo>
-          <StyledTodoList>
-            <List
-              size="large"
-              header={<h1 style={{ margin: 0 }}><Icon type="pushpin" /> To-Do List</h1>}
-              bordered
-              dataSource={todoItems}
-              renderItem={item => <List.Item>{item}</List.Item>}
-            />
-          </StyledTodoList>
-          <StyledForm onSubmit={e => this.addToDoItem(e)}>
-            <Input
-              value={inputValue}
-              onChange={e => this.handleChange(e)}
-              style={{ marginRight: 10 }}
-              placeholder="What do you need to do?"
-            />
-            <Button type="submit">
-              <Icon type="plus" />
-            </Button>
-          </StyledForm>
-        </StyledInfo>
-      </StyledContainer>
-    );
-  }
+  return (
+    <StyledContainer>
+      <StyledInfo>
+        <StyledTodoList>
+          <List
+            size="large"
+            header={<h1 style={{ margin: 0 }}><Icon type="pushpin" /> To-Do List</h1>}
+            bordered
+            dataSource={todoItems}
+            renderItem={(item, i) => <List.Item key={i}>{item}</List.Item>}
+          />
+        </StyledTodoList>
+        <StyledForm onSubmit={e => addItem(e)}>
+          <Input
+            value={inputValue}
+            onChange={e => handleChange(e.target.value)}
+            style={{ marginRight: 10 }}
+            placeholder="What do you need to do?"
+          />
+          <Button type="submit">
+            <Icon type="plus" />
+          </Button>
+        </StyledForm>
+      </StyledInfo>
+    </StyledContainer>
+  );
 }
 
 export default TodoList;
